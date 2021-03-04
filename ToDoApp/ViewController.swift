e48ea46a0bd6a1ee93281bb9df9bab2020d958dd
enespolat24 <enespolat24@gmail.com>
2021-03-05 02:32:41 +0300

    fresh code

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+        
+    var tasks = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //get tasks
+        
     }
 
 
 }
-
+extension ViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: <#T##IndexPath#>, animated: true)
+    }
+}
+extension ViewController : UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: <#T##IndexPath#>)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+}
