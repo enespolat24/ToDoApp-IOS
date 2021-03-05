323dade7a2ff6dee1c1ca95b00a6c109e01a9390
enespolat24 <enespolat24@gmail.com>
2021-03-05 03:31:41 +0300

    fresh code

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -29,11 +29,33 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view.
         //get tasks
+        updateTasks()
     }
+
+    func updateTasks(){
+        tasks.removeAll()
+        guard let count = UserDefaults().value(forKey: "count") as? Int else{
+            return
+        }
+        for x in 0..<count{
+            
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String{
+                tasks.append(task)
+            }
+        }
+        tableView.reloadData()
+        
+    }
+
     @IBAction func didTapAdd(){
         
         let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
         vc.title = "New Task"
+        vc.update = {
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+        }
         navigationController?.pushViewController(vc, animated: true)
         
     }
@@ -41,6 +63,13 @@ class ViewController: UIViewController {
 extension ViewController : UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.title = "New Task"
+      
+        }
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 extension ViewController : UITableViewDataSource{
